739700d2bef494adeb43c7485879e1e2775618b6
Key <springfield0329@gmail.com>
2022-06-24 13:21:55 +0800

    layour rwd finish

---
+++ app/assets/js/all.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".fa-bars").click(function (e) { 
+        console.log("click");
+        $(".menu").toggleClass("menu_show");
+    });
+    $("body").click(function (e) { 
+        if (!$(e.target).hasClass("fa-bars")) {
+        $(".menu").removeClass("menu_show");;
+        }
+    });  
+});
